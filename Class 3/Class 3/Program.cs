18f28b9b7dc76b94d23f432a7ecc4feac4731b82
Stephen Burgess <30017128@student.toiohomai.ac.nz>
2019-03-06 11:44:38 +1300

    Third Commit

---
+++ Class 3/Class 3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter 1 to start");
+            int input = int.Parse(Console.ReadLine());
+
+            if(input ==1)
+           
+
+
+            Console.Write("Please enter your first name:  ");
+            var firstname = (Console.ReadLine());
+
+            Console.Write("Please enter your last name:  ");
+            var lastname = (Console.ReadLine());
+
+            string fullname = firstname + " " + lastname;
+
+
+
+            
+            
+
+            Console.Write("Please enter your age:  ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your gender:  ");
+            string gender = (Console.ReadLine());
+
+            Console.Write("Please enter your address:  ");
+            string address = (Console.ReadLine());
+
+            Console.Write("Please enter a number:  ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter another number:  ");
+            double num2 = double.Parse(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("Your name is:  " + fullname);
+            Console.WriteLine("Your age is:  " + age);
+            Console.WriteLine("Your gender is:  " + gender);
+            Console.WriteLine("Your address is:  " + address);
+
+            double result;
+            result = num1 + num2;
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, result);
+
+            result = num1 - num2;
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, result);
+
+            result = num1 * num2;
+            Console.WriteLine("{0} x {1} = {2}", num1, num2, result);
+
+            result = num1 / num2;
+            Console.WriteLine("{0} ÷ {1} = {2}", num1, num2, result);
+
+
+
+
+            
+            Console.Read();
+
+
+            
+        }
+    }
+}
